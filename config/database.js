c28dc471584fb43195607fdca16a8104a6d05a80
jose <josebetancourtzam18@gmail.com>
2021-10-25 10:27:10 -0400

    Proteccion de Mongo

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.7kgh1.mongodb.net'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapiintranetpira'),
-        username: env('DATABASE_USERNAME', 'piramide'),
-        password: env('DATABASE_PASSWORD', 'Piramide123'),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
